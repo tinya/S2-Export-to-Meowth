82dff231d21b889701185fd852003d949e6920a7
tinya <868903+tinya@users.noreply.github.com>
2019-11-25 15:47:22 -0500

    Made initial gym csv generation more efficient

--- pogomeowthcsv.py
+++ pogomeowthcsv.py
@@ -3,12 +3,12 @@
 import json
 import csv
 
-import_filename = 'gyms+stops_2019_11_21_23_21_19.json'
+import_filename = 'EXPORTNAME.json'
+gymnick_filename = 'gymnicknames.csv'
 exp_gym_filename = 'parsed_gyms.csv'
 exp_stop_filename = 'parsed_stops.csv'
 meowth_gym_filename = 'meowth_gyms.csv'
 meowth_stop_filename = 'meowth_stops.csv'
-parsed_header = ['guid','lat','lon','name','ex']
 meowth_gym_header = ['name','nickname','lat','lon','exraid']
 meowth_stop_header = ['name','nickname','lat','lon']
 
@@ -24,13 +24,17 @@ export_file = open(exp_gym_filename, 'w')
 
 # Create CSV writer object
 csvwriter = csv.writer(export_file)
-csvwriter.writerow(parsed_header)
+csvwriter.writerow(meowth_gym_header)
 
 for gym in gym_data:
  #   print('{} - {}'.format(gym, gym_data[gym]))
  #   for g in gym_data[gym]:
  #       print('{} - {}'.format(g, gym_data[gym][g]))
-    csvwriter.writerow(gym_data[gym].values())
+    #csvwriter.writerow(gym_data[gym].values())
+    if 'isEx' in gym_data[gym].keys():
+        csvwriter.writerow([gym_data[gym]['name'], ' ', gym_data[gym]['lat'], gym_data[gym]['lng'], 'TRUE'])
+    else:
+        csvwriter.writerow([gym_data[gym]['name'], ' ', gym_data[gym]['lat'], gym_data[gym]['lng'], 'FALSE'])
 
 export_file.close()
 
@@ -42,28 +46,40 @@ export_file = open(exp_stop_filename, 'w')
 
 # Create CSV writer object
 csvwriter = csv.writer(export_file)
-csvwriter.writerow(parsed_header)
+csvwriter.writerow(meowth_stop_header)
 
 for stop in stop_data:
-    csvwriter.writerow(stop_data[stop].values())
+    csvwriter.writerow([stop_data[stop]['name'], ' ', stop_data[stop]['lat'], stop_data[stop]['lng']])
 
 export_file.close()
 
-# Convert parsed CSV to Meowth CSV
-export_file = open(meowth_gym_filename, 'w')
-csvwriter = csv.writer(export_file)
-csvwriter.writerow(meowth_gym_header)
 
-with open(exp_gym_filename, 'r') as fd:
-    csvgym = csv.DictReader(fd)
+"""
+# Add nicknames to Meowth CSV
 
-    for row in csvgym:
-        if row['ex'] == 'True':
-            csvwriter.writerow([row['name'], ' ', row['lat'], row['lon'], 'TRUE'])
-        else:
-            csvwriter.writerow([row['name'], ' ', row['lat'], row['lon'], 'FALSE'])
+gymnick_dict = csv.DictReader(open(gymnick_filename, 'r'))
+gym_dict = csv.DictReader(open(exp_gym_filename, 'r'))
+#export_file = open(meowth_gym_filename, 'w')
+#csvwriter = csv.writer(export_file)
 
-export_file.close()
+#csvwriter.writerow(meowth_gym_header)
+
+#with open(exp_gym_filename, 'r') as fd:
+#    csvgym = csv.DictReader(fd)
+
+
+for nick in gymnick_dict:
+    print(nick['name'])
+    tmpgym_dict = list(gym_dict)
+    for gym in tmpgym_dict:
+        print(nick['lat'])
+
+#       if gymnick_dict[nick]['lat'] == gym_dict[gym]['lat'] and gymnick_dict[nick]['lon'] == gym_dict[gym]['lon']:
+#           print(nick['lat'])
+
+#export_file.close()
+
+
 
 export_file = open(meowth_stop_filename, 'w')
 csvwriter = csv.writer(export_file)
@@ -76,3 +92,5 @@ with open(exp_stop_filename, 'r') as fd:
         csvwriter.writerow([row['name'], ' ', row['lat'], row['lon']])
 
 export_file.close()
+
+"""
